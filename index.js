d425abd38d1da0d99942925b43363aabbf058886
Anthony Ellis <amellis245@gmail.com>
2021-11-03 21:36:34 -0400

    created script file and installed inquirer node package

---
+++ index.js
@@ -0,0 +1,13 @@
+// PSEUDO-CODE
+
+// CONSTANTS
+
+// USER INTERACTION
+
+// == PROMPTS
+// == What is your name?
+// == What is your project name?
+// == What is your project about?
+// == What language(s) is your project written in?
+
+// SAVE USER FILE
