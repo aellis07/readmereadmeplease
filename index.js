f5a0141895524b50e3925e6a645eab68a76a410e
Anthony Ellis <amellis245@gmail.com>
2021-11-03 22:48:08 -0400

    added user prompt for username

--- index.js
+++ index.js
@@ -12,6 +12,25 @@ const fs = require("fs");
 
 // == PROMPTS
 // == What is your name?
+var readme = require("inquirer");
+readme
+    .prompt([
+        {
+            // == What is your name?
+
+            type: "input",
+            message: "What is your user name?",
+            name: "username",
+        },
+    ])
+    .then((answers) => {})
+    .catch((error) => {
+        if (error.isTtyError) {
+            // Prompt couldn't be rendered in the current environment
+        } else {
+            // Something else went wrong
+        }
+    });
 // == What is your project name?
 // == What is your project about?
 // == What language(s) is your project written in?
