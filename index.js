b3239634ebea10bd69160a4ace576e3332455f14
Anthony Ellis <amellis245@gmail.com>
2021-11-19 11:43:47 -0500

    created prompt object

--- index.js
+++ index.js
@@ -16,3 +16,78 @@ const readMe = "newReadMe.md";
 
 // == PROMPTS (2)
 // ==============================
+const promptobj = [
+    {
+        // == What is your name?
+
+        type: "input",
+        message: "What is your project name?",
+        name: "projectTitle",
+    },
+    {
+        // == What is your project name?
+
+        type: "input",
+        message: "What is your project purpose?",
+        name: "projectDesc",
+    },
+
+    {
+        // == What is the installation process
+
+        type: "input",
+        message: "Installation instructions",
+        name: "projectInstall",
+    },
+    {
+        // == How do you use the application
+
+        type: "input",
+        message: "How do you use the application associated with your project?",
+        name: "projectUsage",
+    },
+    {
+        // == Project license
+
+        type: "list",
+        message: "Which license is your project available under?",
+        choices: [
+            "MIT License",
+            "GNU General Public License",
+            "Mozilla Public License 2.0",
+            "Unlicense",
+            "Apache License 2.0",
+            "Boost Software License 1.0",
+        ],
+        name: "projectLicense",
+    },
+    {
+        // == Contribution guidlines
+
+        type: "input",
+        message:
+            "What are the guidelines for other developers to contribute to your project?",
+        name: "projectContribute",
+    },
+    {
+        // == How to run test associated with the project?
+
+        type: "input",
+        message: "How do you run any tests associated with your project?",
+        name: "projectTest",
+    },
+    {
+        // == What is your github username?
+
+        type: "input",
+        message: "What is your github username?",
+        name: "projectUserName",
+    },
+    {
+        // == What is your email?
+
+        type: "input",
+        message: "What is your email?",
+        name: "projectEmail",
+    },
+];
